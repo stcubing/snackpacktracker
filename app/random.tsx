e4a56ed40bc6a880b4746730b72dd8fa73245e26
stcubing <scubbing@gmail.com>
2025-12-27 21:59:42 +0800

    hsp randomiser

--- app/random.tsx
+++ app/random.tsx
@@ -9,17 +9,21 @@ export default function random() {
 
     // e.g. 0.1234 will be false true false true
     //      which could translate to rice (false, as opposed to a true chips),
-    //      yes chicken, no beef, yes lamb, etc
+    //      true-false = chicken or whatever, and yes chilli sauce
 
     // number of components:
     // - 1 bool between base
-    // - 3 for meat
+    // - 4 meats (can be determined with 2 bits)
     // - 7 for sauces
     // = 10 decimal points
 
     const [rand, setRand] = useState('');
-    // const [randArray, setRandArray] = useState([]);
+    const [randomArray, setRandomArray] = useState('');
     const randArray: any[] = [];
+
+    const [base, setBase] = useState('');
+    const [meat, setMeat] = useState('');
+    const [sauce, setSauce] = useState('');
     
     // initial
     useEffect(() => {
@@ -30,27 +34,52 @@ export default function random() {
         console.log("--------------");
 
         setRand(() => { // ensure new state of rand is read
-            const newRand: string = Math.random().toPrecision(10);
+            const newRand: string = (Number(Math.random().toPrecision(10)) * Math.pow(10,10)).toString();
             console.log(newRand);
 
-            for (let i = 0; i < 10; i++) {
-                const digit: number = Number(newRand[i+2]);
-                const result: boolean = (digit % 2 == 0) ? true : false;
+            // convert to array
+            for (let i = 0; i < 9; i++) {
+                const result: boolean = (Number(newRand[i]) % 2 == 0) ? true : false;
                 randArray[i] = result;
 
-                // console.log(newRand[i+2], result);
             }
 
             return newRand;
         });
-        console.log(randArray);
+        // console.log(randArray);
+        setRandomArray(randArray.toString());
+
+    
+        // turn random results into hsp
+        setBase((randArray[0]) ? "rice" : "chips");
+
+        if (randArray[1]) {
+            if (randArray[2]) setMeat("chicken"); // truetrue
+            else setMeat("beef"); // truefalse
+        } else {
+            if (randArray[2]) setMeat("lamb"); // falsetrue
+            else setMeat("mixed"); // falsefalse
+        }
+
+        const sauceArray = [];
+        if (randArray[3]) sauceArray.push("bbq");
+        if (randArray[4]) sauceArray.push("chilli");
+        if (randArray[5]) sauceArray.push("garlic");
+        if (randArray[6]) sauceArray.push("mayo");
+        if (randArray[7]) sauceArray.push("hummus");
+        if (randArray[8]) sauceArray.push("tomato");
+        if (randArray[9]) sauceArray.push("sweet chilli");
+        setSauce(sauceArray.join(", "))
 
     }
 
     return (
         <View>
             <Text>{rand}</Text>
-            <Text>{randArray}</Text>
+            <Text>{randomArray}</Text>
+            <Text>{base}</Text>
+            <Text>{meat}</Text>
+            <Text>{sauce}</Text>
             <TextButton text="reroll" onPress={reroll} />
         </View>
     )
